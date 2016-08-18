390d86de4d1dbffcf6abffe02c7632320ae73f4d
Andi Miller <andi@andimiller.net>
2016-08-18 14:00:32 +0100

    add multiplexing test

--- src/test/scala/WaterslideServerSpec.scala
+++ src/test/scala/WaterslideServerSpec.scala
@@ -5,6 +5,10 @@ import org.json4s.JsonDSL._
 import org.json4s.jackson.JsonMethods._
 import org.scalatest.{FlatSpec, MustMatchers}
 
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration._
+import scalaz.stream.wye
+
 class WaterslideServerSpec extends FlatSpec with MustMatchers {
 
   def mockServer = {
@@ -27,6 +31,24 @@ class WaterslideServerSpec extends FlatSpec with MustMatchers {
             """{"diff":[{"op":"replace","path":"/value","value":2}]}"""
         )
     )
+  }
+
+  "WaterslideServer" should "multiplex to multiple streams" in {
+    val s  = mockServer.run
+    val ws = new WaterslideServer("localhost", 9000, "http://localhost:9001/", 2, None)
+    val stream1 = ws.streamIt
+    val stream2 = ws.streamIt
+    val merged = wye(stream1, stream2)(wye.mergeHaltBoth)
+    val expectedResults = List(
+      """{"initial":{"value":1}}""",
+      """{"diff":[{"op":"replace","path":"/value","value":2}]}""",
+      """{"diff":[{"op":"replace","path":"/value","value":3}]}"""
+    )
+    import scala.concurrent.ExecutionContext.Implicits.global
+    val r1 = Future{ stream1.take(3).runLog.run }
+    val r2 = Future{ stream2.take(3).runLog.run }
+    val (results1 :: results2 :: _) = Await.result(Future.sequence(List(r1, r2)), 10 seconds)
+    results1 must equal(results2)
   }
 
 }
