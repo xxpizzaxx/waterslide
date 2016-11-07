2bf5288cb406b3c76ffce2cafc1e76ad5674efa7
Andi Miller <andi@andimiller.net>
2016-11-07 13:58:10 +0000

    scalafmt

--- src/main/scala/WaterslideServer.scala
+++ src/main/scala/WaterslideServer.scala
@@ -33,50 +33,71 @@ import scala.concurrent.Future
 import scalaz.stream.async.mutable.Topic
 import scalaz.stream.time
 
-class WaterslideServer(hostname: String, port: Int, url: String, ttl: Int, metrics: Option[MetricRegistry]) {
+class WaterslideServer(hostname: String,
+                       port: Int,
+                       url: String,
+                       ttl: Int,
+                       metrics: Option[MetricRegistry]) {
   private[this] val log = getLogger
 
   // metrics
   val urlFetch = metrics.map(_.meter("urlfetch"))
-  val tick     = metrics.map(_.meter("tick"))
-  val ping     = metrics.map(_.meter("ping"))
-  val initial  = metrics.map(_.meter("initial"))
-  val unavail  = metrics.map(_.meter("unavailable"))
-  val diff     = metrics.map(_.meter("diff"))
+  val tick = metrics.map(_.meter("tick"))
+  val ping = metrics.map(_.meter("ping"))
+  val initial = metrics.map(_.meter("initial"))
+  val unavail = metrics.map(_.meter("unavailable"))
+  val diff = metrics.map(_.meter("diff"))
 
   // misc http things
   val client = PooledHttp1Client()
-  val OM     = new ObjectMapper()
+  val OM = new ObjectMapper()
 
   // last valid response cache
   val lastValid = TrieMap[String, JsonNode]()
 
-
   val topic: Topic[JsonNode] = scalaz.stream.async.topic[JsonNode](
-    time.awakeEvery(ttl.seconds)(Strategy.DefaultStrategy, DefaultScheduler).map { _ =>
-      urlFetch.foreach(_.mark())
-      client.get[(Status, String)](url) { x =>
-        EntityDecoder.decodeString(x)(Charset.`UTF-8`).map { b =>
-          (x.status, b)
-        }
-      }.run
-    }.filter { case (status, _) =>
-      // we only want Ok results
-      status == Status.Ok
-    }.flatMap { case (_, body) =>
-      // we only want valid JSON
-      Try{OM.readTree(body)}.toOption.map(Process.emit).getOrElse(Process.empty)
-    }
+    time
+      .awakeEvery(ttl.seconds)(Strategy.DefaultStrategy, DefaultScheduler)
+      .map { _ =>
+        urlFetch.foreach(_.mark())
+        client
+          .get[(Status, String)](url) { x =>
+            EntityDecoder.decodeString(x)(Charset.`UTF-8`).map { b =>
+              (x.status, b)
+            }
+          }
+          .run
+      }
+      .filter {
+        case (status, _) =>
+          // we only want Ok results
+          status == Status.Ok
+      }
+      .flatMap {
+        case (_, body) =>
+          // we only want valid JSON
+          Try { OM.readTree(body) }.toOption
+            .map(Process.emit)
+            .getOrElse(Process.empty)
+      }
   )
 
   @volatile var lastValidResponse: JsonNode = null
 
-  topic.subscribe.map(x => lastValidResponse = x).run.runAsync(f => f.leftMap(t => log.error(t)("failed to update the lastValidResponse cache")).rightMap(_ => log.error("lastValid Response cache updater exited with Unit")))
+  topic.subscribe
+    .map(x => lastValidResponse = x)
+    .run
+    .runAsync(
+      f =>
+        f.leftMap(t =>
+            log.error(t)("failed to update the lastValidResponse cache"))
+          .rightMap(_ =>
+            log.error("lastValid Response cache updater exited with Unit")))
 
   def streamIt = {
     val cached = Process.emitAll(Option(lastValidResponse).toList)
     wye(cached, topic.subscribe)(wye.mergeHaltR).zipWithPrevious.filter {
-      case (x, y)          => !x.contains(y) // deduplicate
+      case (x, y) => !x.contains(y) // deduplicate
     }.flatMap { r =>
       val mainResponse = r match {
         // transform to JSON
@@ -95,7 +116,8 @@ class WaterslideServer(hostname: String, port: Int, url: String, ttl: Int, metri
 
   val route = HttpService {
     case _ =>
-      val pings = awakeEvery(10 seconds)(Strategy.DefaultStrategy, DefaultScheduler).map { _ =>
+      val pings = awakeEvery(10 seconds)(Strategy.DefaultStrategy,
+                                         DefaultScheduler).map { _ =>
         ping.foreach(_.mark())
         Ping()
       }
@@ -104,13 +126,17 @@ class WaterslideServer(hostname: String, port: Int, url: String, ttl: Int, metri
       }
       val sink: Sink[Task, WebSocketFrame] = Process.constant {
         case Ping(x) => Task.delay(Pong(x))
-        case f       => Task.delay(println(s"Unknown type: $f"))
+        case f => Task.delay(println(s"Unknown type: $f"))
       }
       val joinedOutput = wye(pings, src)(wye.mergeHaltR)
       WS(Exchange(joinedOutput, sink))
 
   }
 
-  val server = BlazeBuilder.bindHttp(host = hostname, port = port).withWebSockets(true).mountService(route, "/").start
+  val server = BlazeBuilder
+    .bindHttp(host = hostname, port = port)
+    .withWebSockets(true)
+    .mountService(route, "/")
+    .start
 
 }
